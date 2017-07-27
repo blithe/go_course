a414106b5a703b77a4e5f414bb13e1030946a234
blithe <blitherocher@gmail.com>
2017-07-27 15:23:13 -0700

    take a file name and print contents

--- cat/cat.go
+++ cat/cat.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+  "fmt"
+  ioutil "io/ioutil"
+)
+
 func main() {
- distance := 123
- println(distance)
+  Write("text.txt")
+}
+
+func Write(filename string) {
+  contents, err := ioutil.ReadFile(filename)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("%s\n", contents)
 }
